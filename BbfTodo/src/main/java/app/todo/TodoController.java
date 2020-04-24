89f9c9615177ce821f4ad7ff4502c9947c38f4a0
Magnus <battleprobe666@hotmail.com>
2020-04-24 21:45:04 +0200

    Cleaing up imports

--- BbfTodo/src/main/java/app/todo/TodoController.java
+++ BbfTodo/src/main/java/app/todo/TodoController.java
@@ -1,45 +1,45 @@
 package app.todo;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//import java.util.List;
 import java.util.Optional;
-import java.util.stream.StreamSupport;
+//import java.util.stream.StreamSupport;
 
-import javax.validation.Valid;
+//import javax.validation.Valid;
 
 import org.springframework.web.servlet.ModelAndView;
 
 //import com.example.fabrikam.TodoDemo.TodoItem;
 //import com.example.fabrikam.TodoDemo.TodoListViewModel;
-import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.fasterxml.jackson.databind.ObjectMapper;
 
-import org.hibernate.Session;
+//import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
+//import org.springframework.ui.ModelMap;
+//import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+//import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.RestController;
 
-import app.todo.*;
+//import app.todo.*;
 import app.todo.model.Todo;
 
 import app.todo.repository.*;
 
-import app.viewModel.*;
+//import app.viewModel.*;
 
 @Controller
 @RequestMapping("/api")

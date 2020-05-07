4c319411212cf57d99e0630389359de70523b1df
Magnus <battleprobe666@hotmail.com>
2020-05-07 08:30:29 +0200

    Cleaning up and comments

--- BbfTodo/src/main/java/app/todo/TodoController.java
+++ BbfTodo/src/main/java/app/todo/TodoController.java
@@ -1,5 +1,6 @@
 package app.todo;
 
+
 import java.util.Optional;
 import java.util.stream.StreamSupport;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,14 +9,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+ 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import app.todo.model.Todo;
 import app.todo.repository.*;
-import app.viewModel.BusinessModel;
+ 
 
 @Controller
 @RequestMapping("/api")
@@ -31,80 +32,114 @@ public class TodoController {
 
 	@GetMapping("/welcome")
 	public ModelAndView welcome() {
+		// Building up a mv and send it to welcome html
 		ModelAndView mv = new ModelAndView("welcome");
-		mv.addObject("message", "Todo API");
-		// model.addAttribute("message", "Hello and welcome to ProjectTodo");
+		mv.addObject("message", "Todo API, use Postman to execute the commands");
 		return mv;
 	}
-
-	@GetMapping("/getinfoGson")
+	/**
+	 * Method that returns number of "active" todos
+	 * @return
+	 */
+	@GetMapping("/activetodos")
+	 private int activeNumbers() {
+	       return repository.countByCompleted(false);
+	    }
+	
+	/**
+	 * Method that check the repository for a specific id
+	 * @param id
+	 * @return
+	 */
+	@GetMapping("/getinfogson")
 	public @ResponseBody Optional<Todo> getinfo(@RequestParam long id) {
-		//ModelAndView mv = new ModelAndView("welcome");
 		return repository.findById(id);
 	}
-
+/**
+ * Updating a specific todo and all it's attributes
+ * @param id
+ * @param text
+ * @param completed
+ * @return
+ */
 	@RequestMapping("/update2")
-	public @ResponseBody String updateSingleTodo(@RequestParam long id,@RequestParam String text, @RequestParam boolean completed ) {
+	public @ResponseBody String updateSingleTodo(@RequestParam long id, @RequestParam String text,
+			@RequestParam boolean completed) {
 		Todo todo = repository.findById(id).orElse(null);
 		todo.setText(text);
 		todo.setCompletedStatus(completed);
-			repository.save(todo);
+		repository.save(todo);
 		return "Updated todo";
 	}
-@RequestMapping("/update")
-public @ResponseBody String updateThisThing(@ModelAttribute BusinessModel todos) {
-	for(Todo todo : todos.getTodoList()) {
-		Todo element = new Todo(todo.getText());
-		element.setId(todo.getId());
-		element.setCompletedStatus(todo.getCompletedStatus());	
-		repository.save(element);
-	
-	}
-	return "Updated todo";
-}
 
+/**
+ * Deleting a specific post in the repository
+ * @param id
+ * @return
+ */
 	@DeleteMapping("/deletepost")
 	public @ResponseBody String methodDelete(@RequestParam long id) {
-		 
+
 		repository.deleteById(id);
 		return "Post deleted";
 	}
-
+/**
+ * Adding one todo to the repository.
+ * @param text
+ * @return
+ */
 	@PostMapping("/add")
 	public @ResponseBody String addTodoApi(@RequestParam String text) {
 		Todo todo = new Todo(text);
 		repository.save(todo);
 		return "Added todo";
 	}
-
-	
+/**
+ * Dumping the entire repository as json.
+ * @return
+ */
 	@GetMapping(path = "/all")
 	public @ResponseBody Iterable<Todo> getAllDaata() {
 		// This returns a JSON or XML with the users
 		return repository.findAll();
 	}
-
-	@GetMapping(path = "/totalNumber")
+/**
+ * Returning the numbers of todos in repository
+ * @return
+ */
+	@GetMapping(path = "/totalnumber")
 	public @ResponseBody long getNumberTodos() {
 		// This returns a JSON or XML with the users
-		// Getting number of elements in repository
+		// Getting total of number of elements in repository
 		return repository.count();
 	}
 
 //  Important info about spring jpa
 //	https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#reference
-
+/**
+ * Returns all active todos in repository
+ * @param model
+ * @return
+ */
 	@GetMapping(path = "/active")
 	public @ResponseBody Iterable<Todo> getAllActive(Model model) {
 		return repository.findByCompleted(false);
 	}
 
-	//
+/**
+ * Returns completed todos in repository.
+ * @param model
+ * @return
+ */
 	@GetMapping(path = "/done")
 	public @ResponseBody Iterable<Todo> getAllDone(Model model) {
 		return repository.findByCompleted(true);
 	}
-
+/**
+ * Toggles active/done on one todo in repository.
+ * @param id
+ * @return
+ */
 	@RequestMapping("/toggleone")
 	public @ResponseBody String toggleOneElement(@RequestParam long id) {
 		// Getting specific element by Id
@@ -118,8 +153,11 @@ public @ResponseBody String updateThisThing(@ModelAttribute BusinessModel todos)
 		return "Inverting todo status";
 	}
 
-	// @RequestMapping
-	@GetMapping("/toggleAll")
+	/**
+	 * Toggles all todos done/active.
+	 * @return string confirmation
+	 */
+	@GetMapping("/toggleall")
 	public @ResponseBody String toggleAllElements() {
 		Iterable<Todo> elements = repository.findAll();
 		Iterable<Todo> elementsCompleted = repository.findByCompleted(true);
@@ -140,8 +178,29 @@ public @ResponseBody String updateThisThing(@ModelAttribute BusinessModel todos)
 				element.setCompletedStatus(true);
 				repository.save(element);
 			}
-		
+
 		return "Setting all true";
 	}
 
 }
+
+
+//@RequestMapping("/update")
+//public @ResponseBody String updateThisThing(@ModelAttribute BusinessModel todos) {
+//	for (Todo todo : todos.getTodoList()) {
+//		Todo element = new Todo(todo.getText());
+//		element.setId(todo.getId());
+//		element.setCompletedStatus(todo.getCompletedStatus());
+//		repository.save(element);
+//	}
+//	return "Updated todo";
+//}
+
+//@GetMapping("/")
+//public String mainer(Model model) {
+//	ArrayList<Todo> todoList = (ArrayList<Todo>) repository.findAll();
+//	model.addAttribute("NotDoneTodos", activeNumbers());
+//	model.addAttribute("newItem", new Todo());
+//	model.addAttribute("items", new BusinessModel(todoList));
+//	return "index"; // Hmmm change to "todos" --- clean up
+//}

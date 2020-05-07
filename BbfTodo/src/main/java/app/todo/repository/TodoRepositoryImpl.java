2942cdddaad318aff68b6116a4d02ee3bbf649da
Magnus <battleprobe666@hotmail.com>
2020-05-07 08:31:52 +0200

    Cleaned interface

--- BbfTodo/src/main/java/app/todo/repository/TodoRepositoryImpl.java
+++ BbfTodo/src/main/java/app/todo/repository/TodoRepositoryImpl.java
@@ -2,7 +2,7 @@ package app.todo.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
+ 
 import app.todo.model.Todo;;
 @Repository
 public interface TodoRepositoryImpl extends CrudRepository<Todo, Long> {

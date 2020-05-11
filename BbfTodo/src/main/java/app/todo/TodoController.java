d5ab17b0c1a0887c3bfe22afd7ca705d9af65296
GeoMagnus <54456125+MagnusHjornhede@users.noreply.github.com>
2020-05-11 20:18:47 +0200

    Moved responsbody and moved logic from toggleAllElements

--- BbfTodo/src/main/java/app/todo/TodoController.java
+++ BbfTodo/src/main/java/app/todo/TodoController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import app.todo.model.Todo;
 import app.todo.repository.*;
- 
+
 @Controller
 @RequestMapping("/api")
 public class TodoController {
@@ -34,27 +34,29 @@ public class TodoController {
 		mv.addObject("message", "Todo API, use Postman to execute the commands");
 		return mv;
 	}
-	
 
 	/**
 	 * Method that check the repository for a specific id
+	 * 
 	 * @param id
 	 * @return
 	 */
 	@GetMapping("/getinfogson")
-	public @ResponseBody Optional<Todo> getinfo(@RequestParam long id) {
+	@ResponseBody
+	public  Optional<Todo> getinfo(@RequestParam long id) {
 		return repository.findById(id);
 	}
-	
-/**
- * Updating a specific todo and all it's attributes
- * @param id
- * @param text
- * @param completed
- * @return
- */
-	@RequestMapping("/update2")
-	public @ResponseBody String updateSingleTodo(@RequestParam long id, @RequestParam String text,
+
+	/**
+	 * Updating a specific todo and all it's attributes
+	 * 
+	 * @param id
+	 * @param text
+	 * @param completed
+	 * @return
+	 */
+	@RequestMapping("/update")@ResponseBody
+	public String updateSingleTodo(@RequestParam long id, @RequestParam String text,
 			@RequestParam boolean completed) {
 		Todo todo = repository.findById(id).orElse(null);
 		todo.setText(text);
@@ -63,44 +65,54 @@ public class TodoController {
 		return "Updated todo";
 	}
 
-/**
- * Deleting a specific post in the repository
- * @param id
- * @return
- */
+	/**
+	 * Deleting a specific post in the repository
+	 * 
+	 * @param id
+	 * @return
+	 */
 	@DeleteMapping("/deletepost")
-	public @ResponseBody String methodDelete(@RequestParam long id) {
+	@ResponseBody
+	public  String methodDelete(@RequestParam long id) {
 
 		repository.deleteById(id);
 		return "Post deleted";
 	}
-	
-/**
- * Adding one todo to the repository.
- * @param text
- * @return
- */
+
+	/**
+	 * Adding one todo to the repository.
+	 * 
+	 * @param text
+	 * @return
+	 */
 	@PostMapping("/add")
-	public @ResponseBody String addTodoApi(@RequestParam String text) {
+	@ResponseBody
+	public  String addTodoApi(@RequestParam String text) {
 		Todo todo = new Todo(text);
 		repository.save(todo);
 		return "Added todo";
 	}
-/**
- * Dumping the entire repository with .findAll() .
- * @return   All entities.
- */
+
+	/**
+	 * Dumping the entire repository with .findAll() .
+	 * 
+	 * @return All entities.
+	 */
 	@GetMapping(path = "/all")
-	public @ResponseBody Iterable<Todo> getAllDaata() {
+	@ResponseBody
+	public  Iterable<Todo> getAllDaata() {
 		// This returns a JSON or XML with the users
 		return repository.findAll();
 	}
-/**
- * Returning the numbers of todos in repository with count().
- * @return int 
- */
+
+	/**
+	 * Returning the numbers of todos in repository with count().
+	 * 
+	 * @return int
+	 */
 	@GetMapping(path = "/totalnumber")
-	public @ResponseBody long getNumberTodos() {
+	@ResponseBody
+	public long getNumberTodos() {
 		// This returns a JSON or XML with the users
 		// Getting total of number of elements in repository
 		return repository.count();
@@ -108,33 +120,41 @@ public class TodoController {
 
 //  Important info about spring jpa
 //	https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#reference
-/**
- * Returns all active todos in repository with findByCompleted(false) as iterable.
- * @param model  (optional)
- * @return Iterable<Todo> 
- */
+	/**
+	 * Returns all active todos in repository with findByCompleted(false) as
+	 * iterable.
+	 * 
+	 * @param model (optional)
+	 * @return Iterable<Todo>
+	 */
 	@GetMapping(path = "/active")
-	public @ResponseBody Iterable<Todo> getAllActive(Model model) {
+	@ResponseBody
+	public  Iterable<Todo> getAllActive(Model model) {
 		return repository.findByCompleted(false);
 	}
 
-/**
- * Returns completed todos in repository.
- * @param model  (optional)
- * @return Iterable<Todo>
- */
+	/**
+	 * Returns completed todos in repository.
+	 * 
+	 * @param model (optional)
+	 * @return Iterable<Todo>
+	 */
 	@GetMapping(path = "/done")
-	public @ResponseBody Iterable<Todo> getAllDone(Model model) {
+	@ResponseBody
+	public  Iterable<Todo> getAllDone(Model model) {
 		return repository.findByCompleted(true);
 	}
-/**
- * Toggles active/done on one todo in repository.
- * It reads current boolean completed status of the todo and inverts it.  
- * @param id  id of the element to be modified.
- * @return String  confirmation message.
- */
+
+	/**
+	 * Toggles active/done on one todo in repository. It reads current boolean
+	 * completed status of the todo and inverts it.
+	 * 
+	 * @param id id of the element to be modified.
+	 * @return String confirmation message.
+	 */
 	@RequestMapping("/toggleone")
-	public @ResponseBody String toggleOneElement(@RequestParam long id) {
+	@ResponseBody
+	public  String toggleOneElement(@RequestParam long id) {
 		// Getting specific element by Id
 		Todo todo = repository.findById(id).orElse(null);
 		// Inverting the boolean value
@@ -147,23 +167,22 @@ public class TodoController {
 	}
 
 	/**
-	 * Toggles all todos done/active.
-	 * Reading all elements from repository as iterable and reads all elements 
-	 * that are completed in another iterable list.
+	 * Toggles all todos done/active. Reading all elements from repository as
+	 * iterable and reads all elements that are completed in another iterable list.
 	 * These iterables are compared to find out if the entire list is already
-	 * completed or not. If not all elements are completed then the list must 
-	 * toggle completed property as completed. Other case is that the entire list is already 
-	 * is set as completed and then set that as false for every element.
+	 * completed or not. If not all elements are completed then the list must toggle
+	 * completed property as completed. Other case is that the entire list is
+	 * already is set as completed and then set that as false for every element.
 	 * 
 	 * @return string confirmation
 	 */
 	@GetMapping("/toggleall")
-	public @ResponseBody String toggleAllElements() {
+	@ResponseBody
+	public String toggleAllElements() {
 		Iterable<Todo> elements = repository.findAll();
 		Iterable<Todo> elementsCompleted = repository.findByCompleted(true);
 		// If all are completed then set all active
-		if (StreamSupport.stream(elements.spliterator(), false).count() == StreamSupport
-				.stream(elementsCompleted.spliterator(), false).count()) {
+		if (extracted(elements, elementsCompleted)) {
 			{
 				for (Todo element : elements) {
 					// boolean completed = element.getCompletedStatus();
@@ -182,5 +201,35 @@ public class TodoController {
 		return "Setting all true";
 	}
 
-}
+	private boolean extracted(Iterable<Todo> elements, Iterable<Todo> elementsCompleted) {
+		return StreamSupport.stream(elements.spliterator(), false).count() == StreamSupport
+				.stream(elementsCompleted.spliterator(), false).count();
+	}
 
+	
+	/**
+	 * Adding another version of toggle all
+	 * @return String 
+	 */
+	@GetMapping("/toggleallv2")
+	@ResponseBody
+	public String toggleAllElementsV2() {
+		Iterable<Todo> elements = repository.findAll();
+		Iterable<Todo> elementsCompleted = repository.findByCompleted(true);
+		// If all are completed then set all active
+		if (elements.equals(elementsCompleted)) {
+			for (Todo element : elements) {
+				// boolean completed = element.getCompletedStatus();
+				element.setCompletedStatus(false);
+				repository.save(element);
+			}
+		} else {
+			for (Todo element : elements) {
+				// Setting all to true
+				element.setCompletedStatus(true);
+				repository.save(element);
+			}
+		}
+		return "Setting all true";
+	}
+}

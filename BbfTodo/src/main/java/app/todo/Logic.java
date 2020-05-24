bc57c94b3149e89de421b1e6fda23d5692c45a44
GeoMagnus <54456125+MagnusHjornhede@users.noreply.github.com>
2020-05-24 17:23:06 +0200

    Add comment

--- BbfTodo/src/main/java/app/todo/Logic.java
+++ BbfTodo/src/main/java/app/todo/Logic.java
@@ -5,16 +5,15 @@ import java.util.stream.StreamSupport;
 import app.todo.model.Todo;
 import app.todo.repository.TodoRepositoryImpl;
 
+
+// moved logic from controller class to this place
 public class Logic {
 
-	
-	
 	public String extractedRequest(TodoRepositoryImpl repository) throws Exception {
 		Iterable<Todo> elements = repository.findAll();
 		Iterable<Todo> elementsCompleted = repository.findByCompleted(true);
 		if (elements.equals(elementsCompleted)) {
 			for (Todo element : elements) {
-				// boolean completed = element.getCompletedStatus();
 				element.setCompletedStatus(false);
 				repository.save(element);
 			}
@@ -28,10 +27,5 @@ public class Logic {
 		return "Setting all true";
 
 	}
-
-//	private boolean extracted(Iterable<Todo> elements, Iterable<Todo> elementsCompleted) {
-//		return StreamSupport.stream(elements.spliterator(), false).count() == StreamSupport
-//				.stream(elementsCompleted.spliterator(), false).count();
-//	}
 
 }

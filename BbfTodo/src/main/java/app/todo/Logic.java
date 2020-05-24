b7974c957bc668d8f916a5b5f436abd432220ba7
GeoMagnus <54456125+MagnusHjornhede@users.noreply.github.com>
2020-05-24 17:25:35 +0200

    Added comment

--- BbfTodo/src/main/java/app/todo/Logic.java
+++ BbfTodo/src/main/java/app/todo/Logic.java
@@ -9,6 +9,14 @@ import app.todo.repository.TodoRepositoryImpl;
 // moved logic from controller class to this place
 public class Logic {
 
+	
+     /* Toggles all todos done/active. Reading all elements from repository as
+     * iterable and reads all elements that are completed in another iterable list.
+     * These iterables are compared to find out if the entire list is already
+     * completed or not. If not all elements are completed then the list must toggle
+     * completed property as completed. Other case is that the entire list is
+     * already is set as completed and then set that as false for every element.
+     */	
 	public String extractedRequest(TodoRepositoryImpl repository) throws Exception {
 		Iterable<Todo> elements = repository.findAll();
 		Iterable<Todo> elementsCompleted = repository.findByCompleted(true);
